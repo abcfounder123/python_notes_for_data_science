2ef4a5fcb519e7de250c02090213379186ce0818
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-04-19 23:19:14 +0700

    data types

---
+++ day2_data_types.py
@@ -0,0 +1,93 @@
+
+"""
+
+Data Types (15)
+
+1. Character              --->   chr()
+2. Character String       --->   str()
+3. Documentation String   --->   doc
+
+1. Integer                --->   int()
+2. Floating-point number  --->   float()
+3. Complex number         --->   complex()
+
+1. Normal list      list(), tuple()
+2. Unique list      set(), frozenset()
+3. Binary list      bytearray(), bytes()
+4. Number           range()
+5. Item list        dict()
+
+1. Condition        bool()
+
+#################################################
+
+စာ သုံးခု
+နံပါတ် သုံးခု
+စာရင်း ရှစ်ခု
+အခြေအနေ တစ်ခု
+
+ရိုးရိုး စာရင်း
+တန်ဖိုးမထပ်တဲ့ စာရင်း
+binary  စာရင်း
+နံပါတ် စာရင်း
+item စာရင်း
+
+#################################################
+
+1 to 99  =>  range(1, 100, 1)
+start = 1
+stop = 100
+step = 1
+
+1 to 100
+start = 1
+stop = 101
+step = 1
+
+2, 4, 6, 8, ...., 100
+start = 2
+stop = 101
+step = 2
+
+1, 3, 5, 7, ..., 99
+start = 1
+stop = 100
+step = 2
+
+100, 98, 96, ..., 2
+start = 100
+stop = 1
+step = -2
+
+99, 97, 95, ..., 1
+start = 99
+stop = 0
+step = -2
+
+-100, -98, -96, ..., -2
+start = -100
+stop = -1
+step = 2
+
+255, 260, 265, ..., 500   =>  range(255, 501, 5)
+start = 255
+stop = 501
+step = 5
+
+500, 495, 490, ..., 255   =>  range(500, 254, -5)
+start = 500
+stop = 254
+step = -5
+
+0, 1, 2, ..., 10  =>  range(0, 11, 1)
+start = 0
+stop = 11
+step = 1
+
+range(0, 11, 1)
+range(0, 11)         <-- step = 1
+range(11)            <-- start = 0, step = 1
+
+##################################################################################################
+
+"""
