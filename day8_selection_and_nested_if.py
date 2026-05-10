8aa5f41cbefe473ca192c68ab4a89dedc03c6926
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-05-10 23:29:07 +0700

    Selection

---
+++ day8_selection_and_nested_if.py
@@ -0,0 +1,101 @@
+
+"""
+
+Selection
+
+1. Normal Statement ( ; end of line)
+   - motor on
+   - motor off
+   - pass
+   - fail
+
+2. Conditional Statement
+   - If water level is low, motor on.  
+   - If water level is high, motor off. 
+   - if exam pass, show "pass".
+   - if exam fail, show "fail".
+
+3. Conditional if Statement
+   - boolean data type
+   - True
+
+3. Conditional else Statement
+   - boolean data type
+   - False
+   
+################################################
+
+1. Conditional if, if
+2. Code block
+3. Conditional code block
+4. Conditional if code block, if code block, if block 
+5. Conditional else code block, else code block, else block  
+6. Condition
+7. Boolean value
+   - empty => False
+   - any   => True
+8. program flow
+9. control flow
+10. : (code block)
+11. pass (keyword name for pass)
+
+################################################
+
+8. program flow
+
+1. input
+2. assign
+3. input
+4. assign
+5. if
+6. l eq
+7. r eq
+8. and
+9. print
+
+username = input("username = ")
+password = input("password = ")
+if username == "Mg Mg" and password == "12345": print("login successful.")
+
+################################################
+
+Nested if (step.4)
+
+Step.1 (condition => output?) (flow)
+
+- 1010 => adgi
+- 1010 => adgj
+
+################################################
+
+c1 = 1
+c2 = 0
+c3 = 1
+c4 = 0
+
+if c1:
+    print("a")
+    if c2:
+        print("c")
+    else:
+        print("d")
+        if c3:
+            print("g")
+            if c4:
+                print("i")
+            else:
+                print("j")
+        else:
+            print("h")
+
+else:
+    print("b")
+    if c3:
+        print("e")
+    else:
+        print("f")
+
+################################################################################################
+
+"""
+
