a5d49b396a887dc6dd299a12cf58566efee54a0d
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-05-24 23:34:49 +0700

    function

---
+++ day_12_2_knowledge_for_function.py
@@ -0,0 +1,55 @@
+
+"""
+
+Function (10)
+
+1. Introduction
+   - print(), input(), len(), int(input())
+   - Function call =>  print, print()
+   - Why?
+     - Readability, reuse
+     - Decomposition
+   - Where?
+     1. built-in module
+     2. preinstalled module  tkinter.py, math.py, .....
+     3. external module      PyQt5, numpy
+     4. custom function
+
+2. module, package, framwork
+   - function (collection of program)         (one purpose - add(), sqrt() )
+   - module   (file)   (collection of fun)    (one work - math.py, login.py)
+   - package  (folder) (collection of module) (one group - frontend folder)
+   - framework(folder) (collection of package)(one project - Django)
+
+3. Function name
+   - naming rules
+   - same name (last one)
+   - should not give the same name
+
+4. Parameterized function
+   - parameterized function   => def add(x, y):
+   - parameter list           => (x, y), tuple
+   - first parameter          => x
+
+5. Arguments(7)
+   - value passed by function
+   - positional argument  =>  add(1, 2)
+   - keyword argument     =>  add(x=1, y=2)
+   - ...
+
+6. Local, Global, Built-in
+   Local          => local
+   Global         => all (local file)
+   Built-in       => all file
+
+7. Standrd form(3)
+
+8. Types of parameters(6)
+
+9. Passing correct values to function
+
+10. Checking Parameters
+
+##########################################
+
+"""
