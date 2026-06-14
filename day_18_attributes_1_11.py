0503fe0427b35de903b5a20856e77e6115f381f7
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-06-14 23:31:33 +0700

    Create day_18_attributes_1_11.py

---
+++ day_18_attributes_1_11.py
@@ -0,0 +1,376 @@
+
+"""
+Object-oriented Programming
+
+- a paradigm based on the concept of "objects"
+- paradigm ဂရိစကားလုံး တွေးပုံတွေးနည်း
+- concept အယူအဆ / အမြင်
+- object  အရာဝတ္ထု
+
+အရာဝတ္ထုဆိုတဲ့ အယူအဆအပေါ်မှာ အခြေခံထားတဲ့ တွေးပုံတွေးနည်း ဖြစ်ပါတယ်။
+
+အရာဝတ္ထုလို့တွေးပြီး program ရေးတာ ဖြစ်ပါတယ်။
+
+##################################################
+
+Attributes exercises
+
+အရာဝတ္ထုတွေမှာ ကိုယ်ပိုင် ပိုင်ဆိုင်မှုတွေ ရှိကြပါတယ်။
+
+ဒီနေ့သင်ခန်းစာက ပိုင်ဆိုင်မှုနဲ့ပတ်သတ်ပြီး စနစ်တကျ တွေးတတ်ဖို့ ဖြစ်ပါတယ်။
+
+အဆင့်ဆယ်ဆင့်ရှိပြီး ပထမနှစ်ဆင့်ကို လေ့ကျင့်ပြီး အသားကျမှသာ ကျန်တာတွေ ဆက်လေ့လာလို့ရပါမယ်။
+
+##################################################
+
+1. Write
+
+ပထမအဆင့်ကတော့ အတွေးတွေကို ချရေးတာ ဖြစ်ပါတယ်။
+
+အများကြီးနဲ့ ရှုတ်ရှုတ်ထွေးထွေး မတွေးပဲ နည်းနည်းလေးနဲ့ ရိုးရိုးလေးပဲ တွေးရပါမယ်။
+
+ရိုးရိုးလေးတွေးပြီး ရေးတတ်စေချင်တာပါ။
+
+##################################################
+
+2. Divide (data, fun/method)
+
+leg = 2
+hand = 2
+
+def walk():
+
+
+ဒုတိယအဆင့်ကတော့ ရေးထားတာကို ခွဲထုတ်တာ ဖြစ်ပါတယ်။
+
+ခွဲထုတ်တဲ့အချိန်မှာ အတွေးတွေကို ဖျောက်ထားရမှာဖြစ်ပြီး အသစ်ထပ်ထည့်တာတွေ မလုပ်ရပါဘူး။
+
+ရေးထားတဲ့အတိုင်းဖတ်ပြီး စက်ရုပ်လိုမျိုး ခွဲထုတ်ပေးရပါမယ်။
+
+ခွဲထုတ်ရင်းနဲ့ data ဘယ်နှစ်ခု၊ function ဘယ်နှစ်ခု စသဖြင့် အရေအတွက်အပါအဝင် ပေးထားတဲ့အမည်တွေနဲ့ အလုပ်လုပ်ပုံတွေကိုပါ တစ်ခါတည်း အလွတ်ရအောင် မှတ်ထားဖို့လည်း လိုအပ်ပါသေးတယ်။
+
+မှန်မှန်ကန်ကန် ခွဲထုတ်တတ်ဖို့နဲ့ မှတ်တတ်တဲ့အကျင့် ရှိစေချင်တာပါ။
+
+##################################################
+
+Python ၏ အနှစ်သာရ
+
+1. လှတာ ပိုကောင်းတယ်။
+
+2. ရှင်းလင်းတာ ပိုကောင်းတယ်။
+
+3. အတွေးတွေဟာ ရိုးရှင်းနေရမယ်။
+
+###################################################################################################
+
+Object-oriented Programming
+
+- a paradigm based on the concept of "objects"
+
+#################################################
+
+Attributes exercises
+
+Step.1   --->   Write
+
+1. Laptop တွင် serial_no ပါသည်။ on, off လုပ်နိုင်သည်။
+
+2. Network_Card တွင် speed ပါသည်။ သတ်မှတ် speed ဖြင့် download ဆွဲနိုင်သည်။
+
+3. 1995 တွင် ပေါ်ပေါက်ခဲ့သော DialUp ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 9600bit/s ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+4. 1999 တွင် ပေါ်ပေါက်ခဲ့သော ADSL ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 2000000bit/s (2Mbit/s)  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+5. 2006 တွင် ပေါ်ပေါက်ခဲ့သော Ethernet ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10Mbit/s  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+6. 2014 တွင်  Ethernet_2014 ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။   (size, pressure, pump)
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+
+9. Engine တွင် fuel_type ပါသည်။     fuel_type, state, on(), off()  
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+###################################################################################################
+
+Step.2   --->   Divide
+
+class   --->   
+data    --->   
+method  ---> 
+
+1. Laptop တွင် serial_no ပါသည်။ on, off လုပ်နိုင်သည်။
+class   --->   Laptop
+data attributes   --->   serial_no
+method attributes --->   on(), off()
+
+2. Network_Card တွင် speed ပါသည်။ သတ်မှတ် speed ဖြင့် download ဆွဲနိုင်သည်။
+class   --->  Network_Card 
+data    --->  speed 
+method  --->  download() 
+
+3. 1995 တွင် ပေါ်ပေါက်ခဲ့သော DialUp ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 9600bit/s ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   ---> DialUp  
+data    ---> speed = 9600bit/s 
+method  ---> download() 
+
+4. 1999 တွင် ပေါ်ပေါက်ခဲ့သော ADSL ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 2000000bit/s (2Mbit/s)  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+
+class   --->  ADSL 
+data    --->  speed = 2000000bit/s (2Mbit/s) 
+method  --->  download()
+
+5. 2006 တွင် ပေါ်ပေါက်ခဲ့သော Ethernet ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10Mbit/s  ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   --->  Ethernet 
+data    --->  speed = 10Mbit/s 
+method  --->  download()
+
+6. 2014 တွင်  Ethernet_2014 ဟူသော network card အမျိုးအစား၏ download ဆွဲနိုင်သော အမြန်နှုန်း speed သည် 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) ဖြစ်သည်။
+ထိုအမြန်နှုန်းဖြင့် download ဆွဲနိုင်သည်။
+class   --->  Ethernet_2014 
+data    --->  speed = 10000Mbit/s  ( တစ်စက္ကန့်လျှင် 1250 မီဂါဘိုက်) 
+method  --->  download()
+
+7. Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+class   --->  Car 
+data    --->  VIN, tires, engine 
+method  ---> 
+
+8. Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+class   --->  Tires 
+data    --->  size, pressure = 0
+method  --->  pump(p)
+
+def pump():
+    print(f"pump to 0 psi.")
+
+
+def pump(p):
+    print(f"pump to {p} psi.")
+
+
+9. Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+class   --->  Engine  
+data    --->  fuel_type, state = "off"
+method  --->  on(), off()
+
+###################################################################################################
+
+Car မှာ ကားနံပါတ် တာယာနဲ့ အင်ဂျင်ပါတယ်။ (VIN, tires, engine)
+Tires ( ကားတာယာ ) တွင် size နှင့် pressure ပါသည်။
+pressure ၏ မူလတန်ဖိုးသည် 0 ( psi ) ဖြစ်သည်။
+လေထိုးသောလုပ်ဆောင်ချက်ပါသည်။ သတ်မှတ်ပေးလိုက်သော ဖိအားအတိုင်း လေထိုးပေးမည်။
+Engine တွင် fuel_type ပါသည်။
+စက်နှိုး/မနှိုး ဟူသော အခြေအနေ  state ပါသည်။
+မူလအခြေအနေမှာ စက်မနှိုးထားသဖြင့် off ဖြစ်နေမည်။
+ပေးထားသော fuel_type ဖြင့် စက်နှိုး ၊ စက်ရပ် မည့်လုပ်‌ဆောင်ချက်ပါသည်။ ( on(), off() )
+
+class   --->   Car
+data    --->   VIN, tires, engine
+method  --->
+
+class   --->  Tires 
+data    --->  size, pressure = 0 
+method  --->  pump(p)
+
+class   --->  Engine 
+data    --->  fuel_type, state = off 
+method  --->  on(), off()
+
+#################################################
+
+10. Create city_car( "BMW-0001", 16 tires, petrol engine) 
+   pump to 7 psi
+   engine on
+
+
+city_car = Car("BMW-0001", Tires(16), Engine("petrol"))
+city_car.tires.pump(7)
+city_car.engine.on()
+
+#################################################
+
+Step.4   --->   controlling data by fun ( pressure by pump(), state by on() and off() )
+
+self.pressure = p
+
+#################################################
+
+
+Step.5   --->   controlling function by data ( on() and off() by state )
+
+if off: on
+if on: off
+
+
+#################################################
+
+Step.6   --->   Auto create serial number
+
+n = 0
+Car.n += 1
+self.VIN = f"BMW-{Car.n:0>4}"
+
+#################################################
+
+String formatting (fat)
+
+fill character = 0
+alignment      = <   ^   >
+total character count = 4
+
+#################################################
+
+Step.7   --->   Controlling str value (Memory address to Car No)    (20 seconds)
+ 
+"BMW-0001"
+"Petrol Engine"
+"16 Tires"
+
+def __str__(self):
+    return f"<__main__.Car object at {hex(id(self))}>"
+    
+def __str__(self):
+    return self.VIN
+        
+def __str__(self):
+    return f"{self.fuel_type} Engine"  
+        
+def __str__(self):
+    return f"{self.size} Tires"  
+    
+
+<__main__.Car object at 0x104657cb0>      =>  BMW-0001(petrol engine(on))
+<__main__.Tires object at 0x1004f35f0>    =>  15 inches tires
+<__main__.Engine object at 0x10278f680>   =>  petrol engine(on)
+
+def __str__(self):
+    return f"{self.VIN}({self.engine})"
+
+def __str__(self):
+    return f"{self.size} inches tires"
+
+def __str__(self):
+    return  f"{self.fuel_type} engine({self.state})"
+
+
+#################################################
+
+Step.8   --->   Controlling representation str value      (20 seconds)
+
+<__main__.Car object at 0x104657cb0>      =>  BMW-0001(petrol engine(on))
+<__main__.Tires object at 0x1004f35f0>    =>  15 inches tires
+<__main__.Engine object at 0x10278f680>   =>  petrol engine(on)
+
+def __repr__(self):
+    return f"{self.VIN}({self.engine})" 
+
+def __repr__(self):
+    return f"{self.size} inches tires"
+
+def __repr__(self):
+    return f"{self.fuel_type} engine({self.state})"    
+
+################################################# 
+
+Step.9   --->   Creating many object                      (15 seconds)
+
+
+cars = []
+
+for _ in range(100):
+    cars.append(Car(Tires(16), Engine("Petrol")))
+
+#################################################
+
+Step.10   --->   Controlling many object                  (10 seconds)
+
+# last 10 on
+
+for car in cars[-10:]:
+    car.engine.on()
+
+#################################################
+
+Step.11   --->   Reverse engineering                      (30 seconds)
+
+
+
+class Tires:
+    def __init__(self, size):
+        self.size = size
+        self.pressure = 0
+
+    def pump(self, p):
+        self.pressure = p
+        print(f"pump to {p} psi.")
+
+    def __repr__(self):
+        return f"{self.size} inches tires"
+
+
+class Engine:
+    def __init__(self, fuel_type):
+        self.fuel_type = fuel_type
+        self.state = "off"
+
+    def on(self):
+        if self.state == "off":
+            self.state = "on"
+            print("Engine On.")
+        else:
+            print("Already on.")
+            
+    def off(self):
+        if self.state == "on":
+            self.state = "off"
+            print("Engine Off.")
+        else:
+            print("Already off.")
+
+    def __repr__(self):
+        return f"{self.fuel_type} engine({self.state})"
+
+
+class Car:
+    n = 0
+    def __init__(self, tires, engine):
+        Car.n += 1
+        self.VIN = f"BMW-{Car.n:0>4}"
+        self.tires = tires
+        self.engine = engine
+
+    def __repr__(self):
+        return f"{self.VIN}({self.engine})"
+
+
+cars = []
+
+for _ in range(100):
+    cars.append(Car(Tires(16), Engine("Petrol")))
+
+for car in cars[-10:]:
+    car.engine.on()
+    
+print(cars) 
+
+##################################################################################################   
+
+"""
