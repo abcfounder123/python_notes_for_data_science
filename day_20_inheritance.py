31e27acce7e6143536f380de9277aa2aa13f7862
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-06-28 23:54:16 +0700

    Inheritance

--- day_20_inheritance.py
+++ day_20_inheritance.py
@@ -775,11 +775,804 @@ class E(Z):
 
 class X(A, B, C, D, E, F):
     pass
+    
 
 print(X.__mro__)  # X  ABY CDEZ F
+
+#################################################
+
+16. X ၏ attributes ကို ရှာဖွေပါက မည့်သည့်အစီအစဉ်အတိုင်း ရှာဖွေမည်နည်း။
+
+                 
+               Y          Z
+
+             /  \\     /  |  \\    
+
+             A    B    C   D   E   F
+
+               \\   \\ |  /  /  /
+
+                  \\ \\| / / /
+
+                       X                         
+
+Method Resolution Order = X ->  A -> B -> Y  -> C -> D -> E -> Z  -> F
+
+
+class Y:
+    a = "Y"
+    pass
+class Z:
+    a = "Z"
+    pass
+
+class A(Y):
+    a = "A"
+    pass
+class B(Y):
+    a = "B"
+    pass
+
+class C(Z):
+    a = "C"
+    pass
+class D(Z):
+    a = "D"
+    pass
+class E(Z):
+    a = "E"
+    pass
+
+class F:
+    a = "F"
+    pass
+
+class X(A, B, C, D, E, F):
+    a = "X"
+    pass
+
+
+print(X.a) # X
+
+#################################################
+
+17. Method Resolution Order Exercise.1  => X BAY CDEZ F
+
+               Y         Z
+
+             /  \\    /  |  \\
+
+             B   A   C   D   E    F
+
+               \\   \\ |  /  /  /
+
+                  \\ \\| / / /
+
+                       X      
+
+
+class X(B, A, C, D, E, F):
+    pass
+
+#################################################
+
+18. Method Resolution Order Exercise.2  => X CDEZ ABY F 
+
+                Z             Y   
+
+             /  |  \\        / \
+
+            C   D     E     A   B   F
+
+             \\   \\   |   /  /  /
+
+                  \\ \\| / / /
+
+                       X
+
+
+class X(C, D, E, A, B, F):
+    pass
 
 
 #################################################
 
+19. Method Resolution Order Exercise.3  => X F CDEZ ABY
+
+                 Z             Y   
+
+              /  |  \\        / \
+
+       F    C    D     E     A   B  
+
+         \\    \\   \\   |   /  /  
+
+              \\   \\ \\| / / 
+
+                       X
+
+class Y:
+    a = "Y"
+    pass
+class Z:
+    a = "Z"
+    pass
+
+class A(Y):
+    a = "A"
+    pass
+class B(Y):
+    a = "B"
+    pass
+
+class C(Z):
+    a = "C"
+    pass
+class D(Z):
+    a = "D"
+    pass
+class E(Z):
+    a = "E"
+    pass
+
+class F:
+    a = "F"
+    pass
+
+
+class X(F, C, D, E, A, B):
+    a = "X"
+    pass
+
+
+print(X.__mro__)
+
+#################################################
+
+20. Method Resolution Order Exercise.4  
+
+=> Cross diamond example
+=> X  A C BY DEZ F                            
+
+
+                 Y       Z
+
+               /    X    |  \\
+
+              A   C   B  D   E    F
+
+               \\   \\  |  /  /  /
+
+                  \\ \\ | / / /
+
+                        X       
+
+
+class X(A, C, B, D, E, F):
+    pass
+
+
+#################################################
+
+Diamond problem 
+စိန်ပုံစံပျက်စီးလို့ ဖြစ်လာသော ပြဿနာ
+
+consistent MRO
+တစ်ခုကို တစ်ခါစီရှာရင် မှန်ကန်သော ရှာဖွေမှုပုံစံ
+
+inconsistent MRO
+တစ်ခုတည်းကို နှစ်ခါသုံးခါထပ်နေရင် မမှန်ကန်သော ရှာဖွေမှုပုံစံ
+
+ပုံကို ကြည့်ပြီး စိန်ပုံစံမဖြစ်ရင် inconsistent MRO ဖြစ်နေကြောင်း အလွယ်တကူသိနိုင်။
+
+"""
+# A consistent method resolution order (MRO)
+
+#                 Y
+
+#            A   /\   B
+#                \/
+
+#                 X
+
+# A, B သည် Y ဖြစ်သည်။
+# X သည် A, B ဖြစ်သည်။  (A, B, Y)
+"""
+
+class X(A, B):
+    pass
+
+
+class X(Y, A, B):
+    pass
+
+
+"""
+#################################################
+
+# Not a consistent method resolution order
+
+#                 Y
+#              /
+
+#          /    /   \
+#              A      B
+#          \    \   /
+#            \
+#              \
+#                 X  
+
+
+# X သည် Y, A, B ဖြစ်သည်။ (Diamond problem) (Y, A, B, Y)
+# TypeError: Cannot create a consistent method resolution order (MRO)
+
+#################################################  
+"""
+
+class Y:
+    pass
+class Z:
+    pass
+class F:
+    pass
+
+class A(Y):
+    pass
+class B(Y):
+    pass
+
+class C(Z):
+    pass
+class D(Z):
+    pass
+class E(Z):
+    pass
+
+class X(A, B, C, D, E, F):
+    pass
+
+
+print(X.__mro__)
+
+#################################################
+
+21. Method Resolution Order Exercise.5 
+
+
+               Y         Z
+
+             /  \\    /  |  \\
+
+             A   B   C   D   E    F
+
+               \\   \\ |  /  /  /
+
+                  \\ \\| / / /
+
+                       X     
+
+=> G is Y.
+=> X is G.
+=> X AGBY CDEZ F
+
+
+Y
+|
+G      <---
+|
+X
+
+               Y              Z
+
+             /  | \\       /  |  \\
+
+             A  G  B      C   D   E    F
+
+               \\ \\  \\  |  /  /  /
+
+                  \\ \\ \\| / / /
+
+                         X       
+
+class G(Y):
+    pass
+
+
+class X(A, G, B, C, D, E, F):
+    pass
+
+
+#################################################
+
+22. Method Resolution Order Exercise.6 
+
+                Y             Z
+
+             /  | \\       /  |  \\
+
+             A  G  B      C   D   E    F
+
+               \\ \\  \\  |  /  /  /
+
+                  \\ \\ \\| / / /
+
+                         X     
+
+
+=> H is Z.
+=> X is H.
+=> X AGBY CHDEZ F
+
+Z
+|
+H    <---
+|
+X  
+
+"""
+#              Y              Z
+
+#             / | \       / / | \
+
+#             A G  B    C H  D   E   F
+
+#               \ \ \   | | /  /  /
+
+#                  \ \\ | |/ / /
+
+#                        X
+
+"""
+
+class H(Z):
+    pass
+
+
+class X(A, G, B, C, H, D, E, F):
+    pass
+
+
+#################################################
+
+23. Method Resolution Order Exercise.7  
+
+=> X is I.
+=> X AGBY CHDEZ F I
+
+  I
+  |
+  X
+
+
+"""
+#              Y              Z
+
+#             / | \       / / | \
+
+#             A G  B    C H  D  E   F  I
+
+#               \ \ \   | | /  /  / /
+
+#                  \ \\ | |/ /  / /
+
+#                        X
+
+
+"""
+
+class I:
+    pass
+
+
+class X(A, G, B, C, H, D, H, E, F, I):
+    pass
+
+
+#################################################
+
+24. Method Resolution Order Exercise.8  
+
+=> X is J.
+=> X J AGBY CHDEZ F I
+
+  J
+  |
+  X
+
+
+"""
+#              Y              Z
+
+#             / | \       / / | \
+
+#         J  A G  B     C H  D  E   F  I
+
+#           \   \ \ \   | | /  /  / /
+
+#               \  \ \\ | |/ /  / /
+
+#                        X
+
+"""
+
+class J:
+    pass
+
+
+class X(J, A, G, B, C, H, D, E, Z, F, I):
+    pass
+
+
+#################################################
+
+25. Method Resolution Order Exercise.9 
+
+=> All is K.
+
+               Y         Z
+
+             /  \\    /  |  \\  
+
+             A   B   C   D   E     F
+
+               \\   \\ |  /  /  /
+
+                  \\ \\| / / /
+
+                       X   
+
+#################################################
+
+=> All is K.
+
+                     K
+
+
+                /      \\   \\
+
+               Y         Z
+
+             /  \\    /  |  \\  \\
+
+             A   B   C   D   E    F
+
+               \\   \\ |  /  /  /
+
+                  \\ \\| / / /
+
+                       X   
+
+#################################################
+
+class K:
+    a = "K"
+    pass
+
+class Y(K):
+    pass
+class Z(K):
+    pass
+class F(K):
+    pass
+
+class A(Y):
+    pass
+class B(Y):
+    pass
+
+class C(Z):
+    pass
+class D(Z):
+    pass
+class E(Z):
+    pass
+
+class X(A, B, C, D, E, F):  
+    pass
+
+print(X.a)
+
+#################################################
+
+Object Attributes (2)
+
+1. Individual property, Object attribute, Instance attribute
+2. Shared property, Class attribute, Static attribute
+
+'property' refers to 'data & method'.
+'attribute' refers to 'data & method'.
+
+BMW-0001       <--- Individual
+"BMW"          <--- Shared
+
+ID             <--- Individual
+Country name   <--- Shared
+
+#################################################
+
+name, age, ID                  50 + 50 + 50
+country_name = "Myanmar"       1
+precedence = "U BA"            1
+
+1. Memory
+total = 250 millions
+total = 150 millions + 1 + 1
+
+2. Flexiable
+
+p1.country = "Burma"
+p2.country = "Burma"
+...
+p50000000.country = "Burma"      <---  50
+
+citizen.country = "Burma"        <---  1
+
+#################################################
+
+Object attributes နှစ်မျိုး
+
+ပိုင်ဆိုင်မှုတွေ သတ်မှတ်ပေးတဲ့အခါ Oxygen ဘူးလိုမျိုးက တစ်ယောက်ချင်းစီကို အပိုင်ပေးလိုက်လို့ ရပါတယ်။
+
+ဆေးရုံထဲက ပန်းခြံလိုမျိုးကြတော့ လူနာတစ်ယောက်ကို ပန်းခြံတစ်ခုဆိုတာမျိုး ပေးလို့ရပေမယ့် ဖြစ်နိုင်ရင် အတူတူမျှသုံးလိုက်ရင် အကုန်အကျပိုသက်သာပါတယ်။
+
+ဒါကြောင့် object တွေကို Attributes တွေ ပေးတဲ့အခါ Oxygen ဘူးလိုမျိုး ကိုယ်ပိုင်ပစ္စည်းတွေ ရှိသလို ပန်းခြံလိုမျိုး ဘုံပိုင်ဆိုင်မှုတွေလည်း ရှိလာပါတယ်။
+
+နှစ်မျိုးလုံးက object တွေနဲ့ သက်ဆိုင်တာဖြစ်လို့ object attributes လို့ပဲ ခေါ်ကြပါတယ်။
+
+###########
+
+သီခြားပိုင်ဆိုင်မှုတွေကို Individual Property တစ်နည်းအားဖြင့် Instance Attribute (instance variable) လို့ ခေါ်ပါတယ်။
+
+ဘုံပိုင်ဆိုင်မှုတွေကိုတော့ Shared Property တစ်နည်းအားဖြင့် Class Attribute (static variable) လို့ ခေါ်ပါတယ်။
+
+###########
+
+Question .1 
+
+Class attributes ဆိုတာက class ရဲ့ ပိုင်ဆိုင်မှုလား။
+ဒါမှမဟုတ် object တွေရဲ့ ဘုံပိုင်ဆိုင်မှုလား။
+
+Answer : Class level မှာ ရေးထားလို့သာ class attributes လို့ ခေါ်ပေမယ့် အဓိကရည်ရွယ်ချက်က memory သက်သာအောင် မျှသုံးနိုင်ဖို့ ဖြစ်ပါတယ်။
+
+###########
+
+Question.2
+
+Car တစ်စီးမှာ brand အမျိုးအစားနဲ့  VIN number ဆိုတဲ့ ပိုင်ဆိုင်မှုနှစ်ခု ရှိတယ်ဆိုပါစို့။
+ဘယ်ပိုင်ဆိုင်မှုကို မျှပြီး သုံးလို့ ရနိုင်မလဲ။
+
+Answer : ဥပမာ Toyota ကုမ္ပဏီက ထုတ်တဲ့ကားတွေဆိုရင် ကားအစီးရေ ၁၀၀၀ ရှိပါစေ၊ အားလုံးရဲ့ Brand က "Toyota" ပဲ ဖြစ်မှာပါ။ အစီးတိုင်းအတွက် memory ထဲမှာ "Toyota" လို့ လိုက်ရေးနေမယ့်အစား Class Level မှာ တစ်ခါပဲ ရေးထားပြီး အားလုံးက မျှသုံးတာ ပိုထိရောက်ပါတယ်။
+
+VIN ဆိုတာကြတော့ ကားတစ်စီးချင်းစီရဲ့ ကိုယ်ပိုင် မှတ်ပုံတင်နံပါတ် (စက်နံပါတ်) လိုမျိုးပါ။ ကားတစ်စီးနဲ့ တစ်စီး မတူနိုင်တာကြောင့်  Instance Attribute အဖြစ်ပဲ ထားရပါမယ်။
+
+#################################################
+
+26. What should be Class level data?
+
+50 millions people
+
+1. Name       -  obj    =>   500 millions   =>  500 MB
+2. ID         -  obj    =>   500 millions   =>  500 MB
+3. age        -  obj    =>   500 millions   =>  500 MB 
+4. "Myanmar"  -  obj    =>   500 millions   =>  500 MB 
+                                            +   2000 MB
+
+
+1. Name       -  obj    =>   500 millions   =>  500 MB
+2. ID         -  obj    =>   500 millions   =>  500 MB
+3. age        -  obj    =>   500 millions   =>  500 MB 
+4. "Myanmar"  -  cls           1            =>  100 bytes 
+
+                                            +   1500 MB + 100 bytes 
+
+#################################################
+
+{'name': 'p1', 'id': '000000001', 'age': 20, 'country': 'Myanmar'}
+{'name': 'p2', 'id': '000000002', 'age': 30, 'country': 'Myanmar'}
+
+
+class X:
+    def __init__(self, name, id, age, country):
+        self.name = name
+        self.id = id
+        self.age = age 
+        self.country = country
+
+
+p1 = X(name="p1", id="000000001", age=20, country="Myanmar")
+p2 = X(name="p2", id="000000002", age=30, country="Myanmar")
+
+print(p1.__dict__)
+print(p2.__dict__)
+
+p1.country = "Burma"
+p2.country = "Burma"
+
+print(p1.__dict__)
+print(p2.__dict__)
+
+#################################################
+
+{'name': 'p1', 'id': '000000001', 'age': 20}
+{'name': 'p2', 'id': '000000002', 'age': 20}
+
+
+class X:
+    country = "Myanmar"  # class level data
+
+    def __init__(self, name, id, age):
+        self.name = name
+        self.id = id
+        self.age = age
+
+
+p1 = X(name="p1", id="000000001", age=20)
+p2 = X(name="p2", id="000000002", age=20)
+
+print(p1.__dict__)
+print(p2.__dict__)
+
+print(p1.country)
+print(p2.country)
+
+X.country = "Burma"
+
+print(p1.country)
+print(p2.country)
+
+#################################################
+
+{'name': 'p1', 'id': '000000001', 'age': 20}         300 bytes
+{'name': 'p2', 'id': '000000002', 'age': 20}         300 bytes
+
+country = "Myanmar"  =>  100 bytes                   create once, less memory usage
+
+X.country = "Burma"                                  update once, Flexiable
+
+#################################################
+
+27. Use inheritance for the following code.
+
+
+class Male:
+    gender = "male"
+    country = "Myanmar"
+    percedence = "U Ba"
+    system = "Democracy"
+
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+
+class Female:
+    gender = "female"
+    country = "Myanmar"
+    percedence = "U Ba"
+    system = "Democracy"
+
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+
+#################################################
+
+
+class X:
+    country = "Myanmar"
+    percedence = "U Ba"
+    system = "Democracy"
+
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+
+class Male(X):
+    gender = "male"
+
+
+class Female(X):
+    gender = "female"
+
+
+#################################################
+
+p1 = Male(id="123456789", name="Mg Mg", age=20)
+print(p1.gender)
+print(p1.country)
+
+#################################################
+
+28. Create relations for the following classes.
+
+Myanmar Human
+Yangon Human
+Mandalay Human
+America Human
+Newyork Human
+Human
+
+                                Human     head = 1
+                            
+                            /            \\
+                            
+                    Myanmar Human       America Human
+
+                       /    \\                   \\
+                       
+                Yangon Human   Mandalay Human     Newyork Human
+
+
+800 billions , 600 bytes, 100 bytes
+
+obj, class level, inheritance
+
+#################################################
+
+29. Create relations for the following classes.
+
+Car
+Engine Car
+Electric Car
+Diesel Car
+Petrol Car
+
+                       Car
+
+                    /       \\
+
+           Engine Car         Electric Car
+
+           /    \\
+
+   Diesel Car     Petrol Car
+
+#################################################
+
+30. Draw MRO for the following relations.
+
+A is a F.    (ပန်းသီး is a Fruit.)
+T is a F.    (သစ်တော်သီး is a Fruit.)
+O is a F.    (Orange is a Fruit.)
+
+AT is a A.   (ပန်းသစ်တော်သီးသည် ပန်းသီးဖြစ်သည်။)
+AT is a T.   (ပန်းသစ်တော်သီးသည် သစ်တော်သီးဖြစ်သည်။)
+(ပန်းသစ်တော်သီးသည် ပန်းသီးလည်း ဖြစ်သလို သစ်တော်သီးလည်းဖြစ်သည်။)
+
+O1 is a O.
+O2 is a O.
+O3 is a O.
+                             F
+
+                         /   |   \\
+
+                        A    T       O
+
+                        \\   /    /  |  \\
+
+                          AT     O1  O2   O3
+
+#################################################
+
+Knowledges                               
+
+1. comma + space                              x = [1, 2, 3, 4]
+2. operator => s + operator +  s              1 + 2
+3. fun, class  => line 2
+4. while, if, for  =>  line 1
+5. normal program  =>  no line, line 1
+6. class name => UpperCamealCase
+7. all name => lower case, lower_snake_case
+8. constant => UPPER CASE
+
+9. Diamond problam (YAB => YABY)
+10. MRO
+
+##################################################################################################
 
 """
