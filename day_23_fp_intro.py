13f378f211358a810d0fbec29d7a223bd4c40bbe
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-07-05 23:26:26 +0700

    intro

---
+++ day_23_fp_intro.py
@@ -0,0 +1,62 @@
+
+"""
+
+"Introduction of Functional Programming"
+
+x = 1
+
+Procedural programming  => data 1
+int object =>  n=1, repr="1" + 73
+fun => 1
+
+data, fun()
+data + fun()
+fun
+
+-----------------------------------------
+
+1. Procedural Programming            10  and one bottle   (1 bit)
+
+2. OOP                               10  and 10 bottle    (10 * 224 bits)
+
+3. Functional Programming            data pipe line
+
+-----------------------------------------
+
+x = 1
+y = 2
+
+
+def add(x, y):
+   return x + y
+
+
+add(x, y)
+
+-----------------------------------------
+
+
+class int:
+    def __init__(self, n=0):
+        self.n = n
+
+    def __add__(self, other):
+        return int(self.n + other.n)
+
+
+x = int(1)        # n=1, add()
+y = int(2)        # n=2, add()
+
+x.__add__(y)
+x + y
+dollar + kyat  => dollar
+kyat + dollar  => kyat
+
+-----------------------------------------
+
+Imperative    =>   Procedural Programming, OOP
+Declarative   =>   Functional Programming 
+
+----------------------------------------------------------------------------------
+
+"""
