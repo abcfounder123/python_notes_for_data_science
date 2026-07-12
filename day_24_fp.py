5d1b5024a5f17f64764c0bb27b8d60dcf1e4a438
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-07-13 00:39:50 +0700

    LC, HOF, Closure

--- day_24_fp.py
+++ day_24_fp.py
@@ -479,6 +479,394 @@ def a_z():
 
 ##################################################################################
 
+4. Lambda function
+
+အမည်ပေးစရာမလိုတဲ့ တစ်ခါသုံး function ဖြစ်ပါတယ်။
+
+lambda x, y: x + y
+lambda x, y: x - y
+lambda x, y=0: x + y
+lambda x, y, /: x + y
+lambda : print("-" * 42)
+lambda name, age, ph_no: print(name, age, ph_no)
+
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def add(x, y=0):
+    return x + y
+
+
+def add(x, y, /):
+    return x + y
+
+
+def line():
+    print("-" * 42)
+
+
+def info(name, age, ph_no):
+    print(name, age, ph_no)
+
+
+-----------------------------------------
+
+For garbage collection system   
+
+x = 1
+y = 2
+z = x + y
+print(z)
+>> int(1), int(2) ->  int(3)
+>> 28 bytes 28 bytes 28 bytes -> 84 bytes, int 3
+
+print(1 + 2)
+>> 28 bytes 28 bytes 28 bytes -> 0 bytes , int 0
+
+-----------------------------------------
+
+Test purpose
+
+line = lambda :print("-" * 42)
+line()
+
+
+def line():
+    print("-" * 42)
+
+
+line()
+
+##################################################################################
+
+5. Higher-Order Functions
+
+higher ဆိုတာက ပိုကြီးတာကို ပြောတာပါ။ first order function ထက် ပိုကြီးတာတွေပေါ့။
+တနည်းအားဖြင့် second order function, third order function, . . .  တွေကို ပေါင်းပြီး ခေါ်လိုက်တဲ့ အခေါ်အဝေါ်ပေါ့။
+
+သင်္ချာမှာ first order function ဆိုတာက f(x) ကို ပြောတာပါ။
+x တန်ဖိုးထည့်ရင် y တန်ဖိုးထွက်မယ်ဆိုတာမျိုးပါ။
+first order function ထုတ်ပေးရင်တော့ second order function ဖြစ်သွားပါမယ်။
+second order function ထုတ်ပေးရင်တော့ third order function ဖြစ်သွားပါမယ်။
+
+အလွယ်ပြောရရင် function ထုတ်ပေးတဲ့ function မှန်သမျှကို higher-order function လို့ ခေါ်နိုင်ပါတယ်။
+
+f(a, b, c)
+
+f(a)(b)(c)
+
+-----------------------------------------
+
+First-Order Functions -> f(x)
+
+x = 3
+y = 2x = 6
+
+
+def f(x):
+    y = 2 * x
+    return y
+
+-----------------------------------------
+
+Second-Order Functions (produce First-Order Functions)
+
+
+def s(a):
+    def f(x):
+        y = 2 * x
+        return y
+    return f
+
+
+
+s(a) <- Second-Order Functions
+f(x) <- First-Order Functions
+
+-----------------------------------------
+
+Third-Order Functions (produce Second-Order Functions)
+
+def t():
+    def s(a):
+        def f(x):
+            y = 2 * x
+            return y
+
+        return f
+
+    return s
+
+-----------------------------------------
+
+fourth order function
+
+def ff():
+    def t():
+        def s(a):
+            def f(x):
+                y = 2 * x
+                return y
+
+            return f
+        return s
+    return t
+
+-----------------------------------------
+
+"Sample code"
+
+
+def t(a):
+    def s(b):
+        def f(c):
+            y = a * b * c
+            return y
+        return f
+    return s
+
+
+ans = t(2)(3)(4)  # a=2, b=3, c=4
+
+print(ans)
+
+-----------------------------------------
+
+first order function, normal function    -->  data
+
+second order function                    -->  1st function
+third order function                     -->  2nd function
+fourth order function                    -->  3rd function
+
+##################################################################################
+
+6. Closure
+
+Closing something လို့ အဓိပ္ပါယ်ရပါတယ်။
+
+ပုံမှန်အားဖြင့် local data တွေက ဖျက်ခံရပါတယ်။ Closed လုပ်ထားရင်တော့ ဆက်ပြီးရှိနေပါတယ်။
+
+Data တွေကို Global အနေနဲ့ မသုံးချင်တဲ့အခါ local မှာ ထားရင်လည်း ဖျက်ခံရမှာစိုးတဲ့အခါ closure ဆိုတဲ့ နည်းလမ်းကို အသုံးပြုပါတယ်။
+
+- a process of closing somthing
+- closing different data => s(2), s(3), s(7)
+
+- data hiding (closing data)
+- function factories
+- decorators  (closing fun)
+
+-----------------------------------------
+
+
+def s(a):
+
+    def f(x):
+        y = a * x
+        return y
+
+    return f
+
+
+dollar_kyat = s(5000)
+kyat_dollar = s(1/5000)
+dollar_baht = s(35)
+kg_lb = s(2.2)
+
+print(dollar_kyat(10))
+print(kyat_dollar(50000))
+print(dollar_baht(10))
+
+print(dollar_kyat.__closure__)
+print(kg_lb.__closure__)
+
+-----------------------------------------
+
+def s(a):
+
+    def f(x):
+        y = a * x
+        return y
+
+    return f
+
+
+z = s(2)
+
+-----------------------------------------
+
+z = s(2) is same as z = f() that closed a=2
+
+
+def f(x):
+    y = 2 * x
+    return y
+
+z = f
+
+-----------------------------------------
+
+z = s(3) is same as z = f that closed a=3
+
+
+def f(x):
+    y = 3 * x
+    return y
+
+z = f
+
+-----------------------------------------
+
+
+def s(a):
+
+    def f(x):
+        y = a * x
+        return y
+
+    return f
+
+
+multiply_2 = s(2)  # a=2
+print(multiply_2(1))      # 2 * x = 2
+print(multiply_2(2))      # 2 * x = 4
+print(multiply_2(3))      # 2 * x = 6
+
+multiply_3 = s(3)  # a=3
+print(multiply_3(1))      # 3 * x = 3
+print(multiply_3(2))      # 3 * x = 6
+print(multiply_3(3))      # 3 * x = 9
+
+multiply_79 = s(79)
+
+-----------------------------------------
+
+
+def add_factory(n):
+    def add(x):
+        return x + n
+    return add
+
+
+add_1 = add_factory(1) # closed 1
+print(add_1(2))
+
+add_7 = add_factory(7) # closed 7
+print(add_7(2))
+
+-----------------------------------------
+
+def alcohol_permit_germany(age):
+    return age >= 16
+
+def alcohol_permit_england(age):
+    return age >= 19
+
+def alcohol_permit_japan(age):
+    return age >= 21
+
+
+alcohol_permit_germany(20)  =>  True
+alcohol_permit_england      =>  True
+alcohol_permit_japan        =>  False
+
+-----------------------------------------
+
+def alcohol_permit(age):
+    return age >= 16
+
+def alcohol_permit(age):
+    return age >= 19
+
+def alcohol_permit(age):
+    return age >= 21
+
+age by germany = 16
+age by england = 19
+age by japan = 21
+
+def alcohol_permit(age):
+    return age >= age_by_country
+
+-----------------------------------------
+
+Function factories
+
+
+def factory(age_by_country):
+
+    def alcohol_permit(age):
+        return age >= age_by_country
+
+    return alcohol_permit
+
+
+age_by_country = 21  # 19, 16, 21, 18 by location of country
+age = 16             # now - birthyear , 2026 - 2010
+
+alcohol_permit = factory(age_by_country)  # 19, 16, 21, 18
+
+if alcohol_permit(age):
+    print("You can buy.")
+
+else:
+    print("You can not buy.")
+
+-----------------------------------------
+
+Decorators  (closing fun)
+
+
+def f2(x):
+    def f3():
+        print("-" * 41)
+        x()
+        print("-" * 41)
+
+    return f3
+
+
+def f1():
+    print("Hello")
+
+
+z = f2(f1)
+
+print(z.__closure__)
+
+-----------------------------------------
+
+
+def s(a, b, c):
+
+    def f1(x):
+        y = a + x
+        return y
+
+    def f2(x):
+        y = a + b + x
+        return y
+
+    def f3(x):
+        y = a + b + c + x
+        return y
+
+    return f1, f2, f3
+
+
+x, y, z = s(1, 2, 3)
+
+print(s.__closure__)     # None
+print(x.__closure__)     # closed  1
+print(y.__closure__)     # closed  1 and 2
+print(z.__closure__)     # closed  1 and 2 and 3
+
+##################################################################################
 
 """
 
